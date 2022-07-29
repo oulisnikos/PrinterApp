a74519f1fda8251f0b2fc919ce74ce67d98be9da
e.oulis <e.oulis@oss.gr>
2022-07-29 17:32:13 +0300

    Initial Print App

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrintService } from '../services/print.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  bluetoothList: any=[];
+  selectedPrinter: any;
+  testText: string;
 
-  constructor() {}
+  constructor(private print: PrintService) {
+    this.listPrinter();
+  }
+    //This will list all of your bluetooth devices
+  listPrinter() {
+    this.print.searchBluetoothPrinter().then(
+      (resp=>{
+        //List of bluetooth device list
+        this.bluetoothList=resp;
+      })
+    );
+  }
+  //This will store selected bluetooth device mac address
+  selectPrinter(macAddress)
+  {
+    //Selected printer macAddress stored here
+    this.selectedPrinter=macAddress;
+  }
+
+  //This will print
+  printStuff()
+  {
+    //The text that you want to print
+    const myText='Hello hello hello \n\n\n This is a test \n\n\n';
+    this.print.sendToBluetoothPrinter(this.selectedPrinter,this.testText);
+  }
 
 }
